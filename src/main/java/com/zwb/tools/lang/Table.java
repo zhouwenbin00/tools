993bd90a8e60cb44f1f4d90584c91740cf887769
zhouwenbin <2825075112@qq.com>
2019-09-07 14:11:52 +0800

    1

--- src/main/java/com/zwb/tools/lang/Table.java
+++ src/main/java/com/zwb/tools/lang/Table.java
@@ -1,9 +1,49 @@
 package com.zwb.tools.lang;
 
 
+import com.zwb.tools.annotation.Nullable;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @Auther: zhouwenbin
  * @Date: 2019/9/7 13:53
  */
-public class Table {
+public interface Table<R, C, V> {
+    boolean contains(@Nullable R row, @Nullable C column);
+
+    boolean containsRow(@Nullable R row);
+
+    boolean containsColumn(@Nullable C column);
+
+    boolean containsValue(@Nullable V value);
+
+    V get(R row, C column);
+
+    boolean isEmpty();
+
+    int size();
+
+    void clear();
+
+    void putAll(Table<? extends R, ? extends C, ? extends V> table);
+
+    V remove(R row, C column);
+
+    Map<C, V> row(R row);
+
+    Map<R, V> column(C column);
+
+    Set<R> rowKeySet();
+
+    Set<C> columnKeySet();
+
+    Collection<V> values();
+
+    Map<R, Map<C, V>> rowMap();
+
+    Map<C, Map<R, V>> columnMap();
+
 }
